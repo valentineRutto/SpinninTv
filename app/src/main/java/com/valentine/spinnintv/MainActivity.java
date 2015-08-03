92c5e7b99acb6b9585ea07755f6c2922d2d43e91
valentinerutto <rutto28@gmail.com>
2015-08-03 19:36:25 +0300

    added the library on main

--- app/src/main/java/com/valentine/spinnintv/MainActivity.java
+++ app/src/main/java/com/valentine/spinnintv/MainActivity.java
@@ -1,17 +1,53 @@
 package com.valentine.spinnintv;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.youtube.player.YouTubePlayer;
+import com.thefinestartist.ytpa.YouTubePlayerActivity;
+import com.thefinestartist.ytpa.enums.Orientation;
+import com.thefinestartist.ytpa.utils.YouTubeUrlParser;
 
 
 public class MainActivity extends ActionBarActivity {
 
+
+    String VIDEO_ID = YouTubeUrlParser.getVideoId("https://www.youtube" +
+            ".com/watch?v=B5YExaWNTmc&list=PLEE58C6029A8A6ADE");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, YouTubePlayerActivity.class);
+
+// Youtube video ID (Required, You can use YouTubeUrlParser to parse Video Id from url)
+        intent.putExtra(YouTubePlayerActivity.EXTRA_VIDEO_ID, "iS1g8G_njx8");
+
+// Youtube player style (DEFAULT as default)
+        intent.putExtra(YouTubePlayerActivity.EXTRA_PLAYER_STYLE, YouTubePlayer.PlayerStyle.DEFAULT);
+
+// Screen Orientation Setting (AUTO for default)
+// AUTO, AUTO_START_WITH_LANDSCAPE, ONLY_LANDSCAPE, ONLY_PORTRAIT
+        intent.putExtra(YouTubePlayerActivity.EXTRA_ORIENTATION, Orientation.AUTO);
+
+// Show audio interface when user adjust volume (true for default)
+        intent.putExtra(YouTubePlayerActivity.EXTRA_SHOW_AUDIO_UI, true);
+
+// If the video is not playable, use Youtube app or Internet Browser to play it
+// (true for default)
+        intent.putExtra(YouTubePlayerActivity.EXTRA_HANDLE_ERROR, true);
+
+// Animation when closing youtubeplayeractivity (none for default)
+        intent.putExtra(YouTubePlayerActivity.EXTRA_ANIM_ENTER, R.anim.fade_in);
+        intent.putExtra(YouTubePlayerActivity.EXTRA_ANIM_EXIT, R.anim.fade_out);
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+
     }
 
     @Override
